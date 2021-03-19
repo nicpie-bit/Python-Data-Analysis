f4da8576ba83cad920331357e9ceff4a04b754a4
nmbrouss <nicolebroussard@Nicoles-MacBook-Air.local>
2021-03-18 21:33:07 -0400

    Calculated greatest increase and greatest decrease

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,6 +15,7 @@ print('Total Months: ' + str(numberMonths))
 Total = budget_df["Profit/Losses"].sum()
 print('Total: ' + "$" + str(Total))
 
+#Find the Average Change in Profit/Losses
 list_change = []
 i = 1
 j = len(budget_df)
@@ -22,6 +23,23 @@ while i<j:
     change = budget_df["Profit/Losses"][i] - budget_df["Profit/Losses"][i-1]
     list_change.append(change)
     i += 1
+avg_change = sum(list_change) / len(list_change)
+print('Average Change: ' + "$" + str(round(avg_change, 2)))
+
+#Find Greatest increase in profits
+max_increase = max(list_change)
+min_decrease = min(list_change)
+i = 1
+j = len(budget_df)
+while i<j:
+    max_month_inc = (budget_df["Date"][i] == max_increase)
+    min_month_dec = (budget_df["Date"][i] == min_decrease)
+
+print('Greatest Increase in Profits: ' + max_month_inc + ' $' + str(max_increase))
+
+#Find Greatest decrease in losses
+
+print('Greatest Decrease in Losses: ' + min_month_dec + ' $' + str(min_decrease))
 
 
 
